0c2cfbd46bb269b0e89843c8c5b8224b68aecad5
chen4393c <chen4393c@gmail.com>
2018-10-04 21:29:34 -0700

    getting the map displayed

--- app/src/main/java/com/chen4393c/vicinity/main/MapFragment.java
+++ app/src/main/java/com/chen4393c/vicinity/main/MapFragment.java
@@ -2,30 +2,79 @@ package com.chen4393c.vicinity.main;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chen4393c.vicinity.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
+    private static final String TAG = "MapFragment";
+
+    private View mParentView;
+    private MapView mMapView;
 
     public static MapFragment newInstance() {
         MapFragment mapFragment = new MapFragment();
         return mapFragment;
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        mParentView = inflater.inflate(R.layout.fragment_map, container, false);
+        return mParentView;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMapView = (MapView) mParentView.findViewById(R.id.event_map_view);
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();// needed to get the map to display immediately
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mMapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mMapView.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
 }
