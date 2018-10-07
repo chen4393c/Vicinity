9099b8b7d110bea328d7e18a5d834590e814d561
chen4393c <chen4393c@gmail.com>
2018-10-06 21:22:49 -0700

    polishing splash screen

--- app/src/main/java/com/chen4393c/vicinity/SplashActivity.java
+++ app/src/main/java/com/chen4393c/vicinity/SplashActivity.java
@@ -11,9 +11,27 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        Intent intent = new Intent(SplashActivity.this, ControlPanelActivity.class);
-        startActivity(intent);
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, ControlPanelActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
         finish();
     }
 }
