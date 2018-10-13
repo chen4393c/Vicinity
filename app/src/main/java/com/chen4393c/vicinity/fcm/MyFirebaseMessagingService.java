f5d6c8e148f954087abfde79a8e4caf1b2972d67
chen4393c <chen4393c@gmail.com>
2018-10-13 04:11:08 -0700

    error setting icon color

--- app/src/main/java/com/chen4393c/vicinity/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/chen4393c/vicinity/fcm/MyFirebaseMessagingService.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -63,8 +64,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         //Create Notification according to builder pattern
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, "EventReporter");
+
         notificationBuilder
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setColor(getResources().getColor(R.color.colorPrimary))
                 .setContentTitle("FCM Message")
                 .setContentText(fcmmessage)
                 .setAutoCancel(true)
